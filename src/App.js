5a0f9c8075662bf2b9fc5763b9cab4fabf6a9913
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-05-22 11:10:31 +0600

    added responsve navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
+import Navbar from './Component/Navbar';
 import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Navbar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </Navbar>
     </>
   );
 }
---
+++ src/Component/Navbar.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Link, NavLink, useLocation } from 'react-router-dom';
+import { AiOutlineMenuUnfold } from 'react-icons/ai';
+import { MdDashboard } from 'react-icons/md';
+import './style.css';
+
+const Navbar = ({ children }) => {
+  const { pathname } = useLocation();
+
+  const menuItems = (
+    <>
+      <li>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? 'active-link' : 'link')}
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/dashboard"
+          className={({ isActive }) => (isActive ? 'active-link' : 'link')}
+        >
+          Dashboard
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/login"
+          className={({ isActive }) => (isActive ? 'active-link' : 'link')}
+        >
+          Login
+        </NavLink>
+      </li>
+    </>
+  );
+  return (
+    <>
+      <div className="drawer drawer-end">
+        <input id="navbar-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          <div className="w-full navbar min-h-[3.5rem] bg-base-100 sticky top-0 z-50 py-0 lg:px-12 shadow ">
+            {pathname.includes('dashboard') && (
+              <label
+                tabIndex="0"
+                htmlFor="my-drawer-2"
+                className="btn btn-ghost lg:hidden "
+              >
+                <MdDashboard size={25} />
+              </label>
+            )}
+            <div className="flex-1 px-2 mx-2 text-2xl">
+              <Link to={'/'}>Lamps Manufacturer.</Link>
+            </div>
+            <div className="flex-none lg:hidden">
+              <label
+                htmlFor="navbar-drawer"
+                className="btn btn-square btn-ghost"
+              >
+                <AiOutlineMenuUnfold size={25} />
+              </label>
+            </div>
+
+            <div className="flex-none hidden lg:block">
+              <ul className="menu menu-horizontal gap-x-2">{menuItems}</ul>
+            </div>
+          </div>
+          {children}
+        </div>
+        <div className="drawer-side ">
+          <label htmlFor="navbar-drawer" className="drawer-overlay"></label>
+          <ul className="menu p-4 overflow-y-auto w-44 bg-base-100 lg:hidden">
+            {menuItems}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
