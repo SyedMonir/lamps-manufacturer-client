791705f40016ecae21fcb99fef46716aefa45b42
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-05-22 12:42:33 +0600

    added summery section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
+import Summery from './Summery';
 
 const Home = () => {
   return (
     <>
       <Banner />
+      <Summery />
     </>
   );
 };
