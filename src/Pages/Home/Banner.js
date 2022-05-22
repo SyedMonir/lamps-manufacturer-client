e3a550ce5b166c86122a822fd290bb8d02dfb82d
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-05-22 11:31:37 +0600

    added banner

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import hero from '../../assets/image/hero.jpeg';
+
+const Banner = () => {
+  return (
+    <>
+      <div
+        className="hero min-h-screen bg-fixed"
+        style={{
+          backgroundImage: `url(${hero})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">
+              Lamp's Parts Manufacturer Present's
+            </h1>
+            <p className="mb-5">
+              We manufacture and sell the best quality lamps parts in the world.
+              We are the best in the world.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Banner;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
   return (
     <>
-      <h1>HOME</h1>
+      <Banner />
     </>
   );
 };
